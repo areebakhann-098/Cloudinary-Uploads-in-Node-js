5dac718319c147dd036ef7803097301b79011318
areebakhann-098 <areebakhann4595@gmail.com>
2025-11-27 16:34:16 +0500

    update

---
+++ public/files/task1.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const inputFilePath= path.join(__dirname, "input.txt")
+const outputFilePath= path.join(__dirname,  "output.txt")
+
+fs.readFile(inputFilePath ,(err, data)=>{
+    if(err) console.log(err)
+        else{
+    console.log( "file succesfully read", data.toString())}
+});
+fs.writeFile(outputFilePath, "Hi how are you usmaaan ", (err)=>{
+    if(err) console.log(err)
+        else{
+    console.log("data written succesfuly to output.txt")}
+});
+
---
+++ public/streams/task2.js
@@ -0,0 +1,31 @@
+import {createReadStream, createWriteStream, WriteStream} from "fs";
+import path from "path";
+
+const inputFilePath= path.join(import.meta.dirname, "input.txt")
+const outputFilePath= path.join(import.meta.dirname, "output.txt")
+
+const readableStream= createReadStream(inputFilePath, { encoding: "utf8", highWaterMark: 16 });
+const writeableStream= createWriteStream(outputFilePath);
+
+
+
+// readableStream.pipe(writeableStream)
+
+readableStream.on("error", (err)=>console.log(err)) ;
+writeableStream.on("error", (err)=> console.log(err))
+
+
+readableStream.on("data", (chunk)=>{
+  console.log("buffer chunk", Buffer.from(chunk));
+  console.log("Recieved Chunk", chunk)
+  writeableStream.write(chunk)
+
+});
+readableStream.on("end", ()=>{
+  console.log("file read completed")
+  writeableStream.end();
+});
+
+writeableStream.on("finish", () => {
+  console.log("Large file copied successfully using streams!");
+});
